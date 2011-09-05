cf3a990900473c1d5f18e3a811a1ee5076f7c017
ldmotta <ldmotta@visie.com.br>
2011-09-05 11:25:47 -0300

    Language
    - Acrescentando novas descrições de variáveis de language

--- catalog/language/english/payment/pagseguro.php
+++ catalog/language/english/payment/pagseguro.php
@@ -7,7 +7,4 @@
  */
 
 $_['text_title']   = 'PagSeguro';
-$_['text_instruction'] = 'PagSeguro';
-$_['text_payable']     = 'Make Payable To: ';
-$_['text_address']     = 'Enviado para: ';
-$_['text_payment']     = 'Seu pedido não será enviado até que recebamos o pagamento.';
+
